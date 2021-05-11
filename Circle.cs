b2b34b5048e0812f0c726583097fa956ca7ace1f
DenisTromsa <unity.enixan.8@gmail.com>
2021-05-11 08:17:58 +0300

    Second commit

---
+++ Circle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private Transform thisTransform;
+
+    private void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        float start         = -5f;
+        float finish        = 5f;
+        float elapsedTime   = 0f;
+        float time          = 3f;
+
+        yield return new WaitForSeconds(Random.Range(1f, 2f));
+
+        while (elapsedTime < time)
+        {
+            float x = Mathf.Lerp(start, finish, (elapsedTime / time));
+            thisTransform.position = new Vector2(x, thisTransform.position.y);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }            
+    }
+}
---
+++ Moved.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Moved : MonoBehaviour
+{
+
+    [SerializeField] private RectTransform rope;
+    [SerializeField] private GameObject fakeCircle;
+
+    private GrabState grabState = GrabState.Move;
+
+    private void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && grabState != GrabState.Wait)
+        {
+            grabState = GrabState.Grab;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            grabState = GrabState.Wait;
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        float pingPongTime = 0;
+        float startHeight = 250f;
+        float finishHeight = 1500f;
+
+        float waitTime = 0f;
+        float grabTime = 0f;
+
+
+        while (true) {
+            if (grabState == GrabState.Move)
+            {
+                pingPongTime += Time.deltaTime;
+                float zAngle = Mathf.PingPong(pingPongTime * 50f, 100f) - 50f;
+                rope.localRotation = Quaternion.Euler(0,0, zAngle);
+            }
+            else if (grabState == GrabState.Wait)
+            {
+                if (rope.rect.height > startHeight)
+                {
+                    //float y = Mathf.Lerp(startHeight, finishHeight, waitTime);
+                    rope.sizeDelta -= new Vector2(0, 5f);
+                    waitTime += Time.deltaTime;
+                }
+                else
+                    grabState = GrabState.Move;
+            }
+            else if (grabState == GrabState.Grab)
+            {
+                if (rope.rect.height < finishHeight)
+                {
+                    //float y = Mathf.Lerp(finishHeight, startHeight, waitTime);
+                    rope.sizeDelta += new Vector2(0, 5f);
+                    grabTime += Time.deltaTime;
+                }
+                else
+                    grabState = GrabState.Wait;
+            }
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("circle"))
+        {
+            fakeCircle.SetActive(true);
+            collision.gameObject.SetActive(false);
+            grabState = GrabState.Wait;
+            Debug.Log("collision");
+        }
+    }
+}
+
+public enum GrabState
+{
+    Grab,
+    Move,
+    Wait,
+}
